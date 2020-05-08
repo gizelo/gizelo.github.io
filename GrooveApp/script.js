c2a670cf9ef2fdb6abd6007ee6d830ef06c5774f
Robert Gizelo <robertgizelo@Roberts-MacBook-Pro.local>
2020-05-08 17:23:14 +0300

    GrooveApp responsive design added

--- GrooveApp/script.js
+++ GrooveApp/script.js
@@ -3,17 +3,4 @@ $(document).ready(function () {
     $(this).find(".features__block-text").slideToggle();
     $(this).toggleClass("features__block--active");
   });
-  // let currentBlock;
-  // $(".features__block").on("click", function () {
-  //   if(this == currentBlock) {
-  //     $(".features__block-text").slideUp();
-  //     $(".features__block").removeClass("features__block--active");
-  //   } else {
-  //     $(".features__block").removeClass("features__block--active");
-  //     $(".features__block-text").slideUp();
-  //     $(this).find(".features__block-text").slideDown();
-  //     $(this).addClass("features__block--active");
-  //   }
-  //   currentBlock = this;
-  // });
 });
